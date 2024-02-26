a74d416e41efaf11347f605ba72794c31c5c9a0f
hoycmd <145314654+hoycmd@users.noreply.github.com>
2024-02-26 21:37:06 +0700

    Update client.js

--- client.js
+++ client.js
@@ -31,7 +31,7 @@ player.Build.FillQuad.Value = true;
 player.Build.BuildRangeEnable.Value = true;
 Damage.Damageln.Value = false;
 player.Damage.Damageln.Value = false;
-      player.Build.BuildRangeEnable.
+      player.Build.BuildRangeEnable.Value = true;
 Value = true; 
 Damage.Damageln.Value = true;
 Players.Get("9183CF2B463E5CD6").Damage.Damagein.Value = false;
@@ -59,3 +59,70 @@ Inventory.Main.Value = true;
     }
   }
 });
+
+
+var adm = 
+AreaPlayerTriggerService.Get("adm");
+adm.Tags = ["adm"];
+adm.Enable = true;
+adm.OnEnter.Add(function(player){
+player.inventory.Main.Value = true;
+player.inventory.MainInfinity.Value = true;
+player.inventory.Secondary.Value = true;
+player.inventory.SecondaryInfinity.Value = true;
+player.inventory.Melee.Value = true;
+player.inventory.Explosive.Value = true;
+player.inventory.ExplosiveInfinity.Value = true;
+player.inventory.BuildInfinity.Value = true;
+player.inventory.Build.Value = true;
+player.function.fly.Value = true;
+
+player.Ui.Hint.Value = "ТЫ ПОЛУЧИЛ АДМИНКУ!";
+
+
+var VIP =
+AreaPlayerTriggerService.Get("VIP");
+VIP.Tags = ["VIP"];
+VIP.Enable = true;
+VIP.OnEnter.Add(function(player){
+player.inventory.Main.Value = true;
+player.inventory.MainInfinity.Value = true;
+player.inventory.Secondary.Value = true;
+player.inventory.SecondaryInfinity.Value = true;
+player.inventory.Melee.Value = true;
+player.inventory.Explosive.Value = true;
+player.inventory.ExplosiveInfinity.Value = true;
+player.inventory.BuildInfinity.Value = false;
+player.inventory.Build.Value = false;
+player.function.fly.Value = true;
+
+player.Ui.Hint.Value = "ТЫ ПОЛУЧИЛ ВИПКУ!!!";
+
+
+  var editor = 
+  AreaPlayerTriggerService.Get("editor");
+  editor.Tags = ["editor"];
+  editor.Enable = true;
+  editor.OnEnter.Add(function(player){
+  player.Build.Pipette.Value = true;
+player.Build.BalkLenChange.Value = true;
+player.Build.SetSkyEnable.Value = true;
+player.Build.GenMapEnable.Value = true;
+player.Build.ChangeCameraPointsEnable.Value = true;
+player.Build.SetSkyEnable.Value = true;
+player.Build.FloodFill.Value = true;
+player.Build.FillQuad.Value = true;
+player.Build.BuildRangeEnable.Value = true;
+Damage.Damageln.Value = false;
+player.Damage.Damageln.Value = false;
+player.Build.BuildRangeEnable.Value = true;
+
+
+
+  
+  
+
+
+
+  
+
